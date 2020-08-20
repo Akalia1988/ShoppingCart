72aaa998bcb8475eaf751c67b17de2e63a6027cb
arsh <arsh@DESKTOP-CDJ43AB>
2020-08-20 14:56:19 -0500

    added new controllers

---
+++ WebApplication3/Models/Data/Db.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models.Data
+{
+    public class Db : DbContext
+    {
+    }
+}
